6064296db396ef56756915da7b702bd3b6b47eb5
tk250 <55493920+tk250@users.noreply.github.com>
2019-11-07 13:41:00 +0100

    Add files via upload

---
+++ train/net.py
@@ -0,0 +1,179 @@
+# -*- coding: utf-8 -*-
+
+import os
+import cv2
+import torch
+import random
+import numpy as np
+import torch.nn as nn
+from util import util
+import matplotlib.pyplot as plt
+import torch.nn.functional as F
+from config import config
+from got10k.trackers import Tracker
+from network import SiameseAlexNet
+from loss import rpn_smoothL1, rpn_cross_entropy_balance
+
+class TrackerSiamRPN(Tracker):
+
+    def __init__(self, net_path=None, **kargs):
+        super(TrackerSiamRPN, self).__init__(
+            name='SiamRPN', is_deterministic=True)
+
+        '''setup GPU device if available'''
+        self.cuda   = torch.cuda.is_available()
+        print(torch.cuda.is_available())
+        self.device = torch.device('cuda:0' if self.cuda else 'cpu')
+
+        '''setup model'''
+        self.net = SiameseAlexNet()
+        #self.net.init_weights()
+
+        if net_path is not None:
+            self.net.load_state_dict(torch.load(
+                net_path, map_location = lambda storage, loc: storage ))
+        if self.cuda:
+            self.net = self.net.to(self.device)
+
+        '''setup optimizer'''
+        self.optimizer   = torch.optim.SGD(
+            self.net.parameters(),
+            lr           = config.lr,
+            momentum     = config.momentum,
+            weight_decay = config.weight_decay)
+
+    def step(self, epoch, dataset, anchors, i = 0,  train=True):
+
+        if train:
+            self.net.train()
+        else:
+            self.net.eval()
+
+        template, detection, regression_target, conf_target = dataset
+
+        if self.cuda:
+            template, detection = template.cuda(), detection.cuda()
+            regression_target, conf_target = regression_target.cuda(), conf_target.cuda()
+
+        pred_score, pred_regression = self.net(template, detection)
+
+        pred_conf   = pred_score.reshape(-1, 2, config.size).permute(0, 2, 1)
+
+        pred_offset = pred_regression.reshape(-1, 4, config.size).permute(0, 2, 1)
+
+        cls_loss = rpn_cross_entropy_balance(   pred_conf,
+                                                conf_target,
+                                                config.num_pos,
+                                                config.num_neg,
+                                                anchors,
+                                                ohem_pos=config.ohem_pos,
+                                                ohem_neg=config.ohem_neg)
+
+        reg_loss = rpn_smoothL1(pred_offset,
+                                regression_target,
+                                conf_target,
+                                config.num_pos,
+                                ohem=config.ohem_reg)
+
+        loss = cls_loss + config.lamb * reg_loss
+
+        '''anchors_show = anchors
+        exem_img = template[0].cpu().numpy().transpose(1, 2, 0)  # (127, 127, 3)
+        #cv2.imwrite('exem_img.png', exem_img)
+
+        inst_img = detection[0].cpu().numpy().transpose(1, 2, 0) # (255, 255, 3)
+        #cv2.imwrite('inst_img.png', inst_img)
+
+
+
+        topk = 1
+        cls_pred = F.softmax(pred_conf, dim=2)[0, :, 1]
+
+        topk_box = util.get_topk_box(cls_pred, pred_offset[0], anchors_show, topk=topk)
+        img_box = util.add_box_img(inst_img, topk_box, color=(0, 0, 255))
+
+        cv2.imwrite('pred_inst.png', img_box)
+
+        cls_pred = conf_target[0]
+        gt_box = util.get_topk_box(cls_pred, regression_target[0], anchors_show)
+        #print('gt_box', gt_box)
+        img_box = util.add_box_img(img_box, gt_box, color=(255, 0, 0), x = 1, y = 1)
+        #print('gt_box', gt_box)
+        cv2.imwrite('pred_inst_gt.png', img_box)'''
+
+        if train:
+            self.optimizer.zero_grad()
+            loss.backward()
+            torch.nn.utils.clip_grad_norm_(self.net.parameters(), config.clip)
+            self.optimizer.step()
+
+        return cls_loss, reg_loss, loss
+
+    '''save model'''
+    def save(self,model, exp_name_dir, epoch):
+        util.adjust_learning_rate(self.optimizer, config.gamma)
+
+        model_save_dir_pth = '{}/model'.format(exp_name_dir)
+        if not os.path.exists(model_save_dir_pth):
+                os.makedirs(model_save_dir_pth)
+        net_path = os.path.join(model_save_dir_pth, 'model_e%d.pth' % (epoch + 1))
+        torch.save(model.net.state_dict(), net_path)
+
+'''class SiamRPN(nn.Module):
+
+    def __init__(self, anchor_num = 5):
+        super(SiamRPN, self).__init__()
+
+        self.anchor_num = anchor_num
+        self.feature = nn.Sequential(
+            # conv1
+            nn.Conv2d(3, 64, kernel_size = 11, stride = 2),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace = True),
+            nn.MaxPool2d(kernel_size = 3, stride = 2),
+            # conv2
+            nn.Conv2d(64, 192, kernel_size = 5),
+            nn.BatchNorm2d(192),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size = 3, stride = 2),
+            # conv3
+            nn.Conv2d(192, 384, kernel_size = 3),
+            nn.BatchNorm2d(384),
+            nn.ReLU(inplace = True),
+            # conv4
+            nn.Conv2d(384, 256, kernel_size = 3),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace = True),
+            # conv5
+            nn.Conv2d(256, 256, kernel_size = 3),
+            nn.BatchNorm2d(256))
+
+        self.conv_reg_z = nn.Conv2d(256, 256 * 4 * self.anchor_num, 3, 1)
+        self.conv_reg_x = nn.Conv2d(256, 256, 3)
+        self.conv_cls_z = nn.Conv2d(256, 256 * 2 * anchor_num, 3, 1)
+        self.conv_cls_x = nn.Conv2d(256, 256, 3)
+        self.adjust_reg = nn.Conv2d(4 * anchor_num, 4 * anchor_num*1, 1)
+
+    def forward(self, z, x):
+        return self.inference(x, *self.learn(z))
+
+    def learn(self, z):
+        z = self.feature(z)
+        kernel_reg = self.conv_reg_z(z)
+        kernel_cls = self.conv_cls_z(z)
+
+        k = kernel_reg.size()[-1]
+        kernel_reg = kernel_reg.view(4 * self.anchor_num, 256, k, k)
+        kernel_cls = kernel_cls.view(2 * self.anchor_num, 256, k, k)
+
+        return kernel_reg, kernel_cls
+
+    def inference(self, x, kernel_reg, kernel_cls):
+        x = self.feature(x)
+        x_reg = self.conv_reg_x(x)
+        x_cls = self.conv_cls_x(x)
+
+        out_reg = self.adjust_reg(F.conv2d(x_reg, kernel_reg))
+        out_cls = F.conv2d(x_cls, kernel_cls)
+
+        return out_reg, out_cls'''
---
+++ train/train_siamrpn.py
@@ -0,0 +1,201 @@
+# -*- coding: utf-8 -*-
+import os
+import sys
+import json
+import torch
+import random
+import logging
+import argparse
+import numpy as np
+from tqdm import tqdm
+from torch.nn import init
+from config import config
+from net import TrackerSiamRPN
+from data import TrainDataLoader
+from torch.utils.data import DataLoader
+from util import util, AverageMeter, SavePlot
+from got10k.datasets import ImageNetVID, GOT10k
+from torchvision import datasets, transforms, utils
+from custom_transforms import Normalize, ToTensor, RandomStretch, \
+    RandomCrop, CenterCrop, RandomBlur, ColorAug
+
+torch.manual_seed(1234) # config.seed
+
+
+parser = argparse.ArgumentParser(description='PyTorch SiameseRPN Training')
+
+parser.add_argument('--train_path', default='/home/krautsct/RGB-T234', metavar='DIR',help='path to dataset')
+parser.add_argument('--experiment_name', default='default', metavar='DIR',help='path to weight')
+parser.add_argument('--checkpoint_path', default=None, help='resume')
+# /home/arbi/desktop/GOT-10k # /Users/arbi/Desktop # /home/arbi/desktop/ILSVRC
+# 'experiments/default/model/model_e1.pth'
+def main():
+
+    '''parameter initialization'''
+    args = parser.parse_args()
+    exp_name_dir = util.experiment_name_dir(args.experiment_name)
+
+    '''model on gpu'''
+    model = TrackerSiamRPN()
+
+    '''setup train data loader'''
+    name = 'GOT-10k'
+    assert name in ['VID', 'GOT-10k', 'All']
+    if name == 'GOT-10k':
+        root_dir = args.train_path
+        seq_dataset = GOT10k(root_dir, subset='train_i')
+    elif name == 'VID':
+        root_dir = '/home/arbi/desktop/ILSVRC'
+        seq_dataset = ImageNetVID(root_dir, subset=('train'))
+    elif name == 'All':
+        root_dir_vid = '/home/arbi/desktop/ILSVRC'
+        seq_datasetVID = ImageNetVID(root_dir_vid, subset=('train'))
+        root_dir_got = args.train_path
+        seq_datasetGOT = GOT10k(root_dir_got, subset='train')
+        seq_dataset = util.data_split(seq_datasetVID, seq_datasetGOT)
+    print('seq_dataset', len(seq_dataset))
+
+    train_z_transforms = transforms.Compose([
+        ToTensor()
+    ])
+    train_x_transforms = transforms.Compose([
+        ToTensor()
+    ])
+
+    train_data  = TrainDataLoader(seq_dataset, train_z_transforms, train_x_transforms, name)
+    anchors = train_data.anchors
+    train_loader = DataLoader(  dataset    = train_data,
+                                batch_size = config.train_batch_size,
+                                shuffle    = True,
+                                num_workers= config.train_num_workers,
+                                pin_memory = True)
+
+    '''setup val data loader'''
+    name = 'GOT-10k'
+    assert name in ['VID', 'GOT-10k', 'All']
+    if name == 'GOT-10k':
+        val_dir = '/home/krautsct/RGB-t-Val'
+        seq_dataset_val = GOT10k(val_dir, subset='train_i')
+    elif name == 'VID':
+        root_dir = '/home/arbi/desktop/ILSVRC'
+        seq_dataset_val = ImageNetVID(root_dir, subset=('val'))
+    elif name == 'All':
+        root_dir_vid = '/home/arbi/desktop/ILSVRC'
+        seq_datasetVID = ImageNetVID(root_dir_vid, subset=('val'))
+        root_dir_got = args.train_path
+        seq_datasetGOT = GOT10k(root_dir_got, subset='val')
+        seq_dataset_val = util.data_split(seq_datasetVID, seq_datasetGOT)
+    print('seq_dataset_val', len(seq_dataset_val))
+
+    valid_z_transforms = transforms.Compose([
+        ToTensor()
+    ])
+    valid_x_transforms = transforms.Compose([
+        ToTensor()
+    ])
+
+    val_data  = TrainDataLoader(seq_dataset_val, valid_z_transforms, valid_x_transforms, name)
+    val_loader = DataLoader(    dataset    = val_data,
+                                batch_size = config.valid_batch_size,
+                                shuffle    = False,
+                                num_workers= config.valid_num_workers,
+                                pin_memory = True)
+
+    '''load weights'''
+
+    if not args.checkpoint_path == None:
+        assert os.path.isfile(args.checkpoint_path), '{} is not valid checkpoint_path'.format(args.checkpoint_path)
+        checkpoint = torch.load(args.checkpoint_path, map_location='cpu')
+        if 'model' in checkpoint.keys():
+            model.net.load_state_dict(torch.load(args.checkpoint_path, map_location='cpu')['model'])
+        else:
+            model.net.load_state_dict(torch.load(args.checkpoint_path, map_location='cpu'))
+        torch.cuda.empty_cache()
+        print('You are loading the model.load_state_dict')
+
+    elif config.pretrained_model:
+        checkpoint = torch.load(config.pretrained_model)
+        # change name and load parameters
+        checkpoint = {k.replace('features.features', 'featureExtract'): v for k, v in checkpoint.items()}
+        model_dict = model.net.state_dict()
+        model_dict.update(checkpoint)
+        model.net.load_state_dict(model_dict)
+        #torch.cuda.empty_cache()
+
+    '''train phase'''
+    train_closses, train_rlosses, train_tlosses = AverageMeter(), AverageMeter(), AverageMeter()
+    val_closses, val_rlosses, val_tlosses = AverageMeter(), AverageMeter(), AverageMeter()
+
+    train_val_plot = SavePlot(exp_name_dir, 'train_val_plot')
+
+    for epoch in range(config.epoches):
+        model.net.train()
+        if config.fix_former_3_layers:
+                util.freeze_layers(model.net)
+        print('Train epoch {}/{}'.format(epoch+1, config.epoches))
+        train_loss = []
+        with tqdm(total=config.train_epoch_size) as progbar:
+            for i, dataset in enumerate(train_loader):
+
+                closs, rloss, loss = model.step(epoch, dataset,anchors, i,  train=True)
+
+                closs_ = closs.cpu().item()
+
+                if np.isnan(closs_):
+                   sys.exit(0)
+
+                train_closses.update(closs.cpu().item())
+                train_rlosses.update(rloss.cpu().item())
+                train_tlosses.update(loss.cpu().item())
+
+                progbar.set_postfix(closs='{:05.3f}'.format(train_closses.avg),
+                                    rloss='{:05.5f}'.format(train_rlosses.avg),
+                                    tloss='{:05.3f}'.format(train_tlosses.avg))
+
+                progbar.update()
+                train_loss.append(train_tlosses.avg)
+
+                if i >= config.train_epoch_size - 1:
+
+                    '''save model'''
+                    model.save(model, exp_name_dir, epoch)
+
+                    break
+
+        train_loss = np.mean(train_loss)
+
+        '''val phase'''
+        val_loss = []
+        with tqdm(total=config.val_epoch_size) as progbar:
+            print('Val epoch {}/{}'.format(epoch+1, config.epoches))
+            for i, dataset in enumerate(val_loader):
+
+                val_closs, val_rloss, val_tloss = model.step(epoch, dataset, anchors, train=False)
+
+                closs_ = val_closs.cpu().item()
+
+                if np.isnan(closs_):
+                    sys.exit(0)
+
+                val_closses.update(val_closs.cpu().item())
+                val_rlosses.update(val_rloss.cpu().item())
+                val_tlosses.update(val_tloss.cpu().item())
+
+                progbar.set_postfix(closs='{:05.3f}'.format(val_closses.avg),
+                                    rloss='{:05.5f}'.format(val_rlosses.avg),
+                                    tloss='{:05.3f}'.format(val_tlosses.avg))
+
+                progbar.update()
+
+                val_loss.append(val_tlosses.avg)
+
+                if i >= config.val_epoch_size - 1:
+                    break
+
+        val_loss = np.mean(val_loss)
+        train_val_plot.update(train_loss, val_loss)
+        print ('Train loss: {}, val loss: {}'.format(train_loss, val_loss))
+
+
+if __name__ == '__main__':
+    main()
